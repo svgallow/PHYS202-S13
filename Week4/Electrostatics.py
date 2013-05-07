e80f2ae7fc59a1bf3b4b1e68f19f99abfc17697e
svgallow <stewartgalloway@me.com>
2013-05-07 14:01:55 -0700

    Midterm

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -1,23 +1,18 @@
+
 import numpy as np
 
 def pointPotential(x,y,q,posx,posy):
-    k = 8.9875518*10**9 #setting k, q, and d values
-    q = 1*10**-9
-    d=1
-    Vyx = (k*q)/(x**2 + y**2)**(1/2.) #insert Vxy function
-    return Vyx #return Vxy
-x,y = meshgrid(arange(-2.,2.,0.06),arange(-2.,2.,0.06))
-Vyx = pointPotential(x,y,1e-9,0.,0.)
+	k= 8.987551*10**9
+	Vyx = (k*q)/(x**2 + y**2)**(1/2.) #insert Vxy function
+	return Vyx #return Vxy
 
 def dipolePotential(x,y,q,d):
-k = 8.9875518*10**9 #setting k, q, and d values
-q = 1*10**-9
-d=1
-Vxy = (k*q/((x)**2 + (y - d)**2)**(1/2.) - (k*q/(x**2 + (y + d)**2)**(1/2.))) #insert Vxy function
-return Vxy #return Vxy
-x,y = meshgrid(arange(-2.,2.,0.06),arange(-2.,2.,0.06))
-Vxy = dipolePotential(x,y,q,d)
-from mpl_toolkits.mplot3d import Axes3D
-fig = figure()
-ax = fig.gca(projection = '3d')
-p = ax.plot_surface(x,y,Vxy, rstride=1, cstride=1, linewidth=0, cmap=cm.spectral)
+	k = 8.987551*10**9
+	Vxy = (k*q/((x)**2 + (y - d)**2)**(1/2.) - (k*q/(x**2 + (y + d)**2)**(1/2.))) #insert Vxy function
+	return Vxy #return Vxy
+
+def pointField(x,y,q,Xq,Yq):
+	"""Takes arguments (x,y), q, Xq, Yq, and returns a tuple of the electric field components (Ex, Ey)"""
+	k = 8.987551*10**9
+	Ex = k*q*(x - Xq) / ((x - Xq)**2 + (y - Yq)**2)**.5
+	Ey = k*q*(y - Yq) / ((x - Xq)**2 + (y - Y1)**2)**.5
